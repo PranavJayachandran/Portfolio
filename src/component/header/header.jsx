19b5fe68bb67bf9a17401a068683978e8cc3064f
PranjPranav <pranav@tweetscreenr.com>
2023-05-13 15:04:15 +0530

    Changed the summary

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/me-3.png";
-import HeaderSocials  from "./HeaderSocials";
+import HeaderSocials from "./HeaderSocials";
 function header() {
-    return (<header>
-        <div className="container header_container">
-            <h5>Hello I'm</h5>
-            <h1>Pranav Jayachandran</h1>
-            <h5 className="text-light">I do nothing
-            </h5>
-            <CTA/>
-            <HeaderSocials/>
-            <div className="me">
-                <img src={ME} alt="me"/>
-            </div>
-
-            <a href="#contact" className="scroll_down"> Scroll Down</a>
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Pranav Jayachandran</h1>
+        <h5 className="text-light">Web developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
         </div>
-    </header>);
+
+        <a href="#contact" className="scroll_down">
+          {" "}
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 }
-export default header; 
+export default header;
